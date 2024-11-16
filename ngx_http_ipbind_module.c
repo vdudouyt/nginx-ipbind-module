ea6371f4182443ea26b5b569d62c776423ed55bc
vdudouyt <valentin.dudouyt@gmail.com>
2024-11-16 21:59:54 +0200

    Minimal working example

--- ngx_http_ipbind_module.c
+++ ngx_http_ipbind_module.c
@@ -1,6 +1,7 @@
 #include <ngx_config.h>
 #include <ngx_core.h>
 #include <ngx_http.h>
+#include <stdbool.h>
 
 static void *ngx_http_ipbind_create_conf(ngx_conf_t *cf);
 static char *ngx_http_ipbind_zone(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
@@ -13,8 +14,14 @@ typedef struct {
 
 typedef struct {
    // shared memory
-   unsigned int counter;
+   ngx_rbtree_t rbtree;
+   ngx_rbtree_node_t sentinel;
 } ngx_http_ipbind_shctx_t;
+
+typedef struct {
+   ngx_str_node_t node;
+   ngx_str_t addr_text;
+} ngx_http_ipbind_node_t;
 
 static ngx_command_t  ngx_http_ipbind_commands[] = {
    { ngx_string("ipbind_zone"),
@@ -25,7 +32,7 @@ static ngx_command_t  ngx_http_ipbind_commands[] = {
       NULL
    },
    {
-      ngx_string("print_hello"),
+      ngx_string("ipbind"),
       NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1,
       ngx_http_ipbind,
       0,
@@ -63,36 +70,48 @@ ngx_module_t ngx_http_ipbind_module = {
 
 static ngx_int_t ngx_http_ipbind_handler(ngx_http_request_t *r)
 {
+   ngx_log_error(NGX_LOG_INFO, r->connection->log, 0, "uri=%V ip=%V", &r->uri, &r->connection->addr_text);
+
    ngx_http_ipbind_conf_t *ipbindcf = ngx_http_get_module_loc_conf(r, ngx_http_ipbind_module);
+   ngx_slab_pool_t *shpool = (ngx_slab_pool_t *) ipbindcf->shm_zone->shm.addr;
    ngx_http_ipbind_shctx_t *shctx = ipbindcf->shm_zone->data;
-   shctx->counter++;
 
-   u_char *response = ngx_pcalloc(r->pool, 256);
-   ngx_sprintf(response, "Access times: %d", shctx->counter);
-   size_t sz = ngx_strlen(response);
+   ngx_shmtx_lock(&shpool->mutex);
+   uint32_t hash = ngx_hash_key(r->uri.data, r->uri.len);
+   ngx_http_ipbind_node_t *node = (ngx_http_ipbind_node_t *) ngx_str_rbtree_lookup(&shctx->rbtree, &r->uri, hash);
 
-   r->headers_out.content_type.len = strlen("text/html") - 1;
-   r->headers_out.content_type.data = (u_char *) "text/html";
-   r->headers_out.status = NGX_HTTP_OK;
-   r->headers_out.content_length_n = sz;
-   ngx_http_send_header(r);
+   bool ip_check_ok;
 
-   ngx_buf_t    *b;
-   ngx_chain_t   *out;
+   if(node) {
+      ngx_log_error(NGX_LOG_INFO, r->connection->log, 0, "Access record found: uri=%V ip=%V",
+         &node->node.str, &node->addr_text);
+      ip_check_ok = node->addr_text.len == r->connection->addr_text.len && ngx_strncmp(node->addr_text.data, r->connection->addr_text.data, node->addr_text.len) == 0;
+   } else {
+      ngx_log_error(NGX_LOG_INFO, r->connection->log, 0, "URI not accessed yet");
 
-   b = ngx_calloc_buf(r->pool);
+      node = ngx_slab_calloc_locked(shpool, sizeof(ngx_http_ipbind_node_t) + r->uri.len + r->connection->addr_text.len);
+      // TODO: alloc check
+      node->node.node.key = hash;
+      node->node.str.data = (u_char *) node + sizeof(ngx_http_ipbind_node_t);
+      node->node.str.len = r->uri.len;
+      ngx_memcpy(node->node.str.data, r->uri.data, r->uri.len);
 
-   out = ngx_alloc_chain_link(r->pool);
+      node->addr_text.data = (u_char *) node + sizeof(ngx_http_ipbind_node_t) + r->uri.len;
+      node->addr_text.len = r->connection->addr_text.len;
+      ngx_memcpy(node->addr_text.data, r->connection->addr_text.data, r->connection->addr_text.len);
 
-   out->buf = b;
-   out->next = NULL;
+      ngx_rbtree_insert(&shctx->rbtree, &node->node.node);
+      ip_check_ok = true;
+   }
 
-   b->pos = response;
-   b->last = response + sz;
-   b->memory = 1;
-   b->last_buf = 1;
+   ngx_shmtx_unlock(&shpool->mutex);
 
-   return ngx_http_output_filter(r, out);
+   if(!ip_check_ok) {
+      ngx_log_error(NGX_LOG_INFO, r->connection->log, 0,
+         "access denied: %V expected but %V found", &node->addr_text, &r->connection->addr_text);
+   }
+
+   return ip_check_ok ? NGX_DECLINED : NGX_HTTP_FORBIDDEN;
 }
 
 static char *ngx_http_ipbind_zone(ngx_conf_t *cf, ngx_command_t *cmd, void *conf) {
@@ -117,9 +136,10 @@ static char *ngx_http_ipbind_zone(ngx_conf_t *cf, ngx_command_t *cmd, void *conf
 
 static ngx_int_t ngx_http_ipbind_init_zone(ngx_shm_zone_t *shm_zone, void *data) {
    ngx_slab_pool_t *shpool = (ngx_slab_pool_t *) shm_zone->shm.addr;
-   ngx_http_ipbind_shctx_t *sh = ngx_slab_alloc(shpool, sizeof(ngx_http_ipbind_shctx_t));
-   sh->counter = 0;
-   shm_zone->data = sh;
+
+   ngx_http_ipbind_shctx_t *shctx = ngx_slab_alloc(shpool, sizeof(ngx_http_ipbind_shctx_t));
+   shm_zone->data = shctx;
+   ngx_rbtree_init(&shctx->rbtree, &shctx->sentinel, ngx_str_rbtree_insert_value);
    return NGX_OK;
 }
 
